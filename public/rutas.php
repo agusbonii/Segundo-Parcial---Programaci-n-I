41bfe8531c96a021217c4fe9cd96c6519d63a7f3
Agustin Bonilla <abonilla@netlabsHG>
2022-07-07 15:49:46 -0300

    Se agregan mas rutas para nuevas funciones

--- public/rutas.php
+++ public/rutas.php
@@ -1,10 +1,21 @@
 <?php 
     require "../utils/autoload.php";
 
-    Routes::AddView("/","login");
+    Routes::AddView("/","index");
     Routes::AddView("/login","login");
+    Routes::AddView("/settings","settings");
     Routes::Add("/login","post","SesionControlador::IniciarSesion");
+    Routes::Add("/logout","get","SesionControlador::CerrarSesion");
+    
     Routes::AddView("/usuarios/alta","usuarios/alta");
+    Routes::Add("/usuarios/alta","post","UsuarioControlador::Alta");
+
+    Routes::AddView("/usuarios/modificacion","usuarios/modificacion");
+    Routes::Add("/usuarios/modificacion","post","UsuarioControlador::modificacion");
+
+    Routes::AddView("/usuarios/baja","usuarios/baja");
+    Routes::Add("/usuarios/baja","post","UsuarioControlador::Baja");
+
     Routes::Run();
 
        
