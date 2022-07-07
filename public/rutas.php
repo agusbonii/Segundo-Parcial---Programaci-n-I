3f5df2c34161ba9b81189cc5b144d07ec798e89c
Agustin Bonilla <abonilla@netlabsHG>
2022-07-07 08:00:00 -0300

    Se agrega ruta para login desde raiz

--- public/rutas.php
+++ public/rutas.php
@@ -1,6 +1,7 @@
 <?php 
     require "../utils/autoload.php";
 
+    Routes::AddView("/","login");
     Routes::AddView("/login","login");
     Routes::Add("/login","post","SesionControlador::IniciarSesion");
     
