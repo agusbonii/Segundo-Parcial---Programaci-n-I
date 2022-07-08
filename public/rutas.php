6b9d05cb670d8ce6495fe2b3cb6e015a81789f52
Agustin Bonilla <abonilla@netlabsHG>
2022-07-08 18:15:52 -0300

    Se prepara para realizar pull requests

--- public/rutas.php
+++ public/rutas.php
@@ -1,20 +1,16 @@
 <?php 
     require "../utils/autoload.php";
 
-    Routes::AddView("/","index");
     Routes::AddView("/login","login");
-    Routes::AddView("/settings","settings");
+    Routes::AddView("/","login");
+    Routes::AddView("/inicio","publicacion/inicio");
+    Routes::AddView("/publicacion/alta","publicacion/alta");
+    Routes::AddView("/publicacion/mispublicaciones","publicacion/misPublicaciones");
+
     Routes::Add("/login","post","SesionControlador::IniciarSesion");
-    Routes::Add("/logout","get","SesionControlador::CerrarSesion");
-    
-    Routes::AddView("/usuarios/alta","usuarios/alta");
-    Routes::Add("/usuarios/alta","post","UsuarioControlador::Alta");
-
-    Routes::AddView("/usuarios/modificacion","usuarios/modificacion");
-    Routes::Add("/usuarios/modificacion","post","UsuarioControlador::modificacion");
-
-    Routes::AddView("/usuarios/baja","usuarios/baja");
-    Routes::Add("/usuarios/baja","post","UsuarioControlador::Baja");
+    Routes::Add("/publicacion/alta","post","PostBlogControlador::Alta");
+    Routes::Add("/publicacion/baja?id=" . $_GET["id"] ,"get","PostBlogControlador::Baja");
+    Routes::Add("/publicacion/modificacion","post","PostBlogControlador::Modificacion");
 
     Routes::Run();
 
